197cb1ec207088a04d7f3853b261f86e7a49bc51
maiKone <michaelmrmj@gmail.com>
2022-09-28 21:47:02 -0300

    .

--- src/modules/accounts/useCases/autheticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/autheticateUser/AuthenticateUserUseCase.ts
@@ -1,3 +1,5 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -8,6 +10,14 @@ interface IRequest {
     password: string;
 }
 
+interface IResponse {
+    user: {
+        name: string
+        email: string
+    };
+    token: string;
+}
+
 @injectable()
 class AuthenticateUserUseCase {
     constructor(
@@ -16,13 +26,34 @@ class AuthenticateUserUseCase {
     ) { }
 
 
-    async execute({ email, password }: IRequest) {
+    async execute({ email, password }: IRequest): Promise<IResponse> {
         const user = await this.usersRepository.findByEmail(email);
 
 
         if (!user) {
             throw new Error("Email or password incorrect");
         }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const token = sign({}, "few7f6ew87vwv8w7v8ewvg", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            }
+        }
+
+        return tokenReturn
     }
 
 }
