54a02273c05c8406cc876ab948bf92250d395944
maiKone <michaelmrmj@gmail.com>
2022-10-27 21:08:00 -0300

    27102022

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,13 +5,14 @@ import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositor
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "../../modules/cars/infra/typeorm/repositories/CarsRepository";
 
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
 );
-
 
 
 container.registerSingleton<ISpecificationsRepository>(
@@ -23,4 +24,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
-)
+);
+
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
+);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../typeorm";
+import createConnection from "../typeorm";
 
 import "../../container";
 
@@ -11,6 +11,8 @@ import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
 
+
+createConnection();
 const app = express();
 
 app.use(express.json());
