fb3c58ad8a551e8f3a082dbe133646373089d50f
maiKone <michaelmrmj@gmail.com>
2022-11-08 22:37:34 -0300

    08112022

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -12,6 +12,37 @@ describe("list Cars", () => {
     });
 
     it("should be able to list all available cars", async () => {
-        await listCarsUseCase.execute();
+
+        const car = await carsRepositoryInMemory.create({
+            name: "Fusca",
+            description: "manco",
+            daily_rate: 10,
+            license_plate: "aasd-234",
+            fine_amount: 20,
+            brand: "VW",
+            category_id: "category_id",
+        });
+
+        const cars = await listCarsUseCase.execute({});
+        expect(cars).toEqual([car])
+    });
+
+
+
+    it("should be able to list all available cars by name", async () => {
+        const car = await carsRepositoryInMemory.create({
+            name: "Fusc3a",
+            description: "manco",
+            daily_rate: 10,
+            license_plate: "aasd-234",
+            fine_amount: 20,
+            brand: "VW",
+            category_id: "category_id",
+        });
+
+        const cars = await listCarsUseCase.execute({
+            brand: "Car_Brand"
+        });
+        expect(cars).toEqual([car])
     })
 })
